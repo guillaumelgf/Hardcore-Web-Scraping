c9ec35b89d76af9663124ca4541daf238949a400
Guillaume lgf <guillaume.lgf@gmail.com>
2018-09-24 09:45:26 +0200

    new version, less messy

--- main.py
+++ main.py
@@ -1,135 +1,162 @@
-import win32api, win32con, win32clipboard
-import time
-import ctypes
+import datetime
+import requests
+import json
+
+#--- General varialbles ---
+dayToNumber = {"monday": 0, "tuesday": 1, "wednesday": 2, "thirsday": 3, "friday": 4, "saturday": 5, "sunday": 6}
+payloadLanPar = {"origin":"Lannion","originCode":"FRLAI","originLocation":{"id":None,"label":None,"longitude":None,"latitude":None,"type":None,"country":None,"stationCode":"FRLAI","stationLabel":None},"destination":"Paris (Toutes gares intramuros)","destinationCode":"FRPAR","destinationLocation":{"id":None,"label":None,"longitude":None,"latitude":None,"type":None,"country":None,"stationCode":"FRPAR","stationLabel":None},"via":None,"viaCode":None,"viaLocation":None,"directTravel":False,"asymmetrical":False,"professional":False,"customerAccount":False,"oneWayTravel":True,"departureDate":"2018-09-28T06:00:00","returnDate":None,"travelClass":"SECOND","country":"FR","language":"fr","busBestPriceOperator":None,"passengers":[{"travelerId":None,"profile":"YOUNG","age":12,"birthDate":None,"fidelityCardType":"NONE","fidelityCardNumber":None,"commercialCardNumber":"","commercialCardType":"YOUNGS","promoCode":"","lastName":None,"firstName":None,"phoneNumer":None,"hanInformation":None}],"animals":[],"bike":"NONE","withRecliningSeat":False,"physicalSpace":None,"fares":[],"withBestPrices":False,"highlightedTravel":None,"nextOrPrevious":False,"source":"SHOW_NEXT_RESULTS_BUTTON","targetPrice":None,"han":False,"outwardScheduleType":"BY_DEPARTURE_DATE","inwardScheduleType":"BY_DEPARTURE_DATE","currency":None,"codeFce":None,"companions":[],"asymetricalItinerary":None}
+payloadParLan = {"origin":"Paris (Toutes gares intramuros)","originCode":"FRPAR","originLocation":{"id":None,"label":None,"longitude":None,"latitude":None,"type":None,"country":None,"stationCode":"FRPAR","stationLabel":None},"destination":"Lannion","destinationCode":"FRLAI","destinationLocation":{"id":None,"label":None,"longitude":None,"latitude":None,"type":None,"country":None,"stationCode":"FRLAI","stationLabel":None},"via":None,"viaCode":None,"viaLocation":None,"directTravel":False,"asymmetrical":False,"professional":False,"customerAccount":False,"oneWayTravel":True,"departureDate":"2018-09-30T06:00:00","returnDate":None,"travelClass":"SECOND","country":"FR","language":"fr","busBestPriceOperator":None,"passengers":[{"travelerId":None,"profile":"YOUNG","age":12,"birthDate":None,"fidelityCardType":"NONE","fidelityCardNumber":None,"commercialCardNumber":"","commercialCardType":"YOUNGS","promoCode":"","lastName":None,"firstName":None,"phoneNumer":None,"hanInformation":None}],"animals":[],"bike":"NONE","withRecliningSeat":False,"physicalSpace":None,"fares":[],"withBestPrices":False,"highlightedTravel":None,"nextOrPrevious":False,"source":"FORM_SUBMIT","targetPrice":None,"han":False,"outwardScheduleType":"BY_DEPARTURE_DATE","inwardScheduleType":"BY_DEPARTURE_DATE","currency":None,"codeFce":None,"companions":[],"asymetricalItinerary":{}}
+url = "https://www.oui.sncf/proposition/rest/search-travels/outward"
+#--------------------------
 
 
-#First let's define the positions of the buttons
-list_positions = [
-	(1069, 400),
-	(1069, 510),
-	(1069, 628),
-	(1069, 741),
-	(1069, 845),
-	(1069, 400)]
-
-#Then generating the inputs -------------------------------------------
-SendInput = ctypes.windll.user32.SendInput
+#--- Variables to set ---
+numDays = 	11 * 7 			#Total number of days to collect
+weekDay = 	"friday"		#Day of the weeek to collect
+departureLoc = "Lannion"
+offline = True
+#------------------------
 
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+#--- Usefull functions ---
+def datetimeToPost(date):
+	year = str(date.year)
+	month = str(date.month)
+	day = str(date.day)
+	if len(month) == 1:
+		month = "0"+month
+	if len(day) == 1:
+		day = "0"+day
 
-# C struct redefinitions 
-PUL = ctypes.POINTER(ctypes.c_ulong)
-class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),
-                ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
+	hour = str(date.hour)
+	minute = str(date.minute)
+	if len(hour) == 1:
+		hour = "0"+hour
+	if len(minute) == 1:
+		minute = "0"+minute
 
-class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong),
-                ("wParamL", ctypes.c_short),
-                ("wParamH", ctypes.c_ushort)]
-
-class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
-
-class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("ii", Input_I)]
-
-# Actuals Functions
-
-def PressKey(hexKeyCode):
-
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( hexKeyCode, 0x48, 0, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-def ReleaseKey(hexKeyCode):
-
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( hexKeyCode, 0x48, 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-
-def copy():
-    PressKey(0x11) #Ctrl
-    PressKey(0x41) #C
-    ReleaseKey(0x41) #~C
-    ReleaseKey(0x11) #~Ctrl
-    PressKey(0x11) #Ctrl
-    PressKey(0x43) #C
-    ReleaseKey(0x43) #~C
-    ReleaseKey(0x11) #~Ctrl
-
-#if __name__ =="__main__":
-#	click(10, 10)
-#	time.sleep(1)
-#	run()
-#-------------------------------------------------------------------------------
-
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-
-def previousPage():
-	click(20,53)
-
-def setup():
-	#focus page
-	click(10, 10)
-	#click first
-	click(1067, 625)
-	#wait loading
-	time.sleep(2)
-	previousPage()
-	#all set
-
-def getFromClipBoard():
-	win32clipboard.OpenClipboard()
-	data = win32clipboard.GetClipboardData()
-	win32clipboard.CloseClipboard()
-	return data
-
-setup()
-click(10, 10)
-time.sleep(2)
-count = 0
-getFromClipBoard()
-for x, y in list_positions:
+	date = year+"-"+month+"-"+day+"T"+hour+":"+minute+":00"	
 	
-	click(x,y)
-	time.sleep(2)
+	return date
 
-	copy()
-	print(count)
-	data = getFromClipBoard()
+def postToDatetime(date):
+	date = date.split("T")
+	hours = date[1].split(":")
+	date = date[0].split("-")
 
-	file_name = "datas/novembre/file"+str(count)
-	count+=1
-	file = open(file_name, "w") 
-	file.write(data)
-	file.close() 
+	year = date[0]
+	month = date[1]
+	day = date[2]
 
-	previousPage()
-	time.sleep(2)
+	hour = hours[0]
+	minute = hours[1]
+
+	date = {
+		"date": {
+			"year": year,
+			"month": month,
+			"day": day
+		},
+		"hours": {
+			"hour": hour,
+			"minute": minute
+		}
+	}
+
+	return date
+
+def getDatas(payload, offline=False):
+	if offline:
+		# Version Offline
+		with open(payload) as file:
+			datas = json.load(file)
+		datas = datas["trainProposals"]
+	else:
+		# Version Online
+		response = requests.post(url, json=payload)
+		datas = json.loads(response.text)["trainProposals"]
+	return datas
+#-------------------------
+
+
+# I - Get the list of days to collect
+weekDay = dayToNumber[weekDay]
+now = datetime.datetime.now()
+base = datetime.datetime(now.year, now.month, now.day, 6, 0, 0)
+date_list = [base + datetime.timedelta(days=x) for x in range(0, numDays) if (base + datetime.timedelta(days=x)).weekday() == 4]
+
+
+
+# II - Get the list of informations for the trains for all days
+final_list = []
+for date in date_list:
+	
+	# x.1 Select payload ---
+	if departureLoc == "Lannion":
+		payload = payloadLanPar
+	elif departureLoc == "Paris":
+		payload = payloadParLan
+
+
+	# x.2 Get the first part of the datas ---
+	payload["departureDate"] = datetimeToPost(date)
+	payloadOff = "response/response1.json" # for offline only
+	trains_part1 = getDatas(payloadOff, offline=offline)
+
+
+	# x.3 Get the second part of the datas ---
+	#Increment the last date of 1 minute
+	nbrTrains = len(trains_part1)
+	date = trains_part1[nbrTrains-1]["departureDate"]
+	date = postToDatetime(date)
+	date = datetime.datetime(int(date["date"]["year"]), int(date["date"]["month"]), int(date["date"]["day"]), int(date["hours"]["hour"]), int(date["hours"]["minute"])+1, 0)
+	date = datetimeToPost(date)
+	payload["departureDate"] = date
+	payloadOff = "response/response2.json"
+	trains_part2 = getDatas(payloadOff, offline=offline)
+
+
+	# x.4 Combine both datasets ---
+	trains = trains_part1 + trains_part2
+
+
+	# x.5 Select only important datas
+	for train in trains:
+		# For only one train
+		departureDate = postToDatetime(train["departureDate"])
+		arrivalDate = postToDatetime(train["arrivalDate"])
+		minuteDuration = train["minuteDuration"]
+		priceProposals = train["priceProposals"]
+
+		for price in priceProposals:
+			amount = price["amount"]
+			typeT = price["type"]
+			travelClass1 = price["segmentProposals"][0]["travelClass"]
+			travelClass2 = price["segmentProposals"][1]["travelClass"]
+			remainingSeat1 = price["passengerDetails"][0]["quotations"][0]["remainingSeat"]
+			remainingSeat2 = price["passengerDetails"][0]["quotations"][1]["remainingSeat"]
+
+			train_dict = {
+				"departureDate": departureDate,
+				"arrivalDate": arrivalDate,
+				"minuteDuration": minuteDuration,
+
+				"amount": amount,
+				"type": typeT,
+				"travelClass1": travelClass1,
+				"travelClass2": travelClass2,
+				"remainingSeat1": remainingSeat1,
+				"remainingSeat2": remainingSeat2
+			}
+			final_list.append(train_dict)
+
+
+# III - Write the datas in file
+dataToWrite = {"trainProposals": final_list}
+now = datetime.datetime.now()
+now = datetime.datetime(now.year, now.month, now.day, now.hour, now.minute, 0)
+now = datetimeToPost(now).replace(":", "-")
+file_name = "new_datas/"+now+".json"
+
+with open(file_name, "w") as file:
+	json.dump(dataToWrite, file)
